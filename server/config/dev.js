9a8e6930cf346fdbe73a45250431773840ad7ad1
Kevin Saephanh <Kevin_Saephanh@yahoo.com>
2019-09-05 17:08:01 -0700

    Test

---
+++ server/config/dev.js
@@ -0,0 +1,9 @@
+const devKeys = {
+    SECRET: "SUPERDUPERSECRETKEY",
+    MONGODB_URI:
+        "mongodb+srv://kevinsaephanh:LeagueofLegends7@cluster0-djuzp.mongodb.net/test?retryWrites=true&w=majority",
+    MAIL_USER: "algoprotoday@gmail.com",
+    MAIL_PASS: "Baldangle7"
+};
+
+module.exports = devKeys;
